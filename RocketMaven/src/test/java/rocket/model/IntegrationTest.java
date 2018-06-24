fddb2b6c3f58b8ac56abd4db22e63afbfcb11379
Pieed <Pieed@192.168.0.59>
2018-06-24 17:25:28 +0200

    renate schafft das

---
+++ RocketMaven/src/test/java/rocket/model/IntegrationTest.java
@@ -0,0 +1,5 @@
+package rocket.model;
+
+public class IntegrationTest {
+
+}
---
+++ RocketMaven/src/test/java/rocket/model/PlanetTest2.java
@@ -0,0 +1,13 @@
+package rocket.model;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(IntegrationTest.class)
+public class PlanetTest2 {
+
+	@Test
+	public void doesNotFail() throws Exception {
+		System.out.println("das ist ein test");
+	}
+}
